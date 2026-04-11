3791492eeafdbc845900ce9924ced45df9061859
VisionSapkota <sapkotavision90@gmail.com>
2026-04-11 16:43:08 +0545

    Issues fix

--- components/GetStarted.jsx
+++ components/GetStarted.jsx
@@ -6,11 +6,7 @@ const GetStarted = () => {
         <div>
             <Link href="/login">
                 <button className="px-6 py-3 bg-purple-600 text-white rounded-xl text-lg hover:bg-purple-700 transition cursor-pointer">
-<<<<<<< HEAD
-                    Login
-=======
                     Go to Login
->>>>>>> 83ad582034468384225e2e14a8f753d44f184e2e
                 </button>
             </Link>
         </div>
